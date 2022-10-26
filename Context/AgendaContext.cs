d8b4fef958ac6c52396ac6cc848eed43409e4733
Jean Carlos <jcpds14@gmail.com>
2022-10-26 17:57:07 -0300

    add migrations and update database

--- Context/AgendaContext.cs
+++ Context/AgendaContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using mvc.Models;
+#nullable disable
 
 namespace mvc.Context
 {
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AgendaContext>(options =>
-options.UseSqlServer("ConexaoPadrao"));
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllersWithViews();
 
